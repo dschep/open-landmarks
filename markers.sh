17095303fb17dcc0b953442cf6c5780c263b8e60
dschep <dschep@gmail.com>
2024-07-27 18:42:49 +0000

    Deploying to gh-pages from @ dschep/open-landmarks@613211ed962bfd99e891244f05c5a057e38027eb 🚀

--- markers.sh
+++ markers.sh
@@ -9,7 +9,7 @@ for wikidata_id in $(jq '.[].wikidata' landmarks.json  -r); do
     echo $wikidata_id
     curl -L -o image "https://hub.toolforge.org/$wikidata_id?p=image&w=800"
     file image
-    magick convert image image.jpg || continue
+    convert image image.jpg || continue
     inkscape --export-type=png marker.svg
     mv marker.png wikidata_icons/$wikidata_id.png
     echo
